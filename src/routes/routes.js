d861b94fd652a48ab671fdb02e6efe1ee28776b0
DDR23 <roboreserva.andre@gmail.com>
2024-04-25 20:42:15 -0300

    fix:{Syntax correction in routes.js}

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,32 +4,32 @@ function routes(app) {
   app.use('/', require('./index'));
 
   //PRODUCTS
-  app.use('/product', require('./product/postProduct'))
-  app.use('/product', require('./product/getProduct'))
-  app.use('/product', require('./product/getOneProduct'))
-  app.use('/product', require('./product/putProduct'))
-  app.use('/product', require('./product/deleteProduct'))
+  app.use('/product', require('./product/postProduct'));
+  app.use('/product', require('./product/getProduct'));
+  app.use('/product', require('./product/getOneProduct'));
+  app.use('/product', require('./product/putProduct'));
+  app.use('/product', require('./product/deleteProduct'));
 
   //INVENTORY
-  app.use('/inventory', require('./inventory/postInventory'))
-  app.use('/inventory', require('./inventory/getInventory'))
-  app.use('/inventory', require('./inventory/getOneInventory'))
-  app.use('/inventory', require('./inventory/putInventory'))
-  app.use('/inventory', require('./inventory/deleteInventory'))
+  app.use('/inventory', require('./inventory/postInventory'));
+  app.use('/inventory', require('./inventory/getInventory'));
+  app.use('/inventory', require('./inventory/getOneInventory'));
+  app.use('/inventory', require('./inventory/putInventory'));
+  app.use('/inventory', require('./inventory/deleteInventory'));
   
   //CLIENT
-  app.use('/client', require('./client/postClient'))
-  app.use('/client', require('./client/getClient'))
-  app.use('/client', require('./client/getOneClient'))
-  app.use('/client', require('./client/putClient'))
-  app.use('/client', require('./client/deleteClient'))
+  app.use('/client', require('./client/postClient'));
+  app.use('/client', require('./client/getClient'));
+  app.use('/client', require('./client/getOneClient'));
+  app.use('/client', require('./client/putClient'));
+  app.use('/client', require('./client/deleteClient'));
   
   //SALE
-  app.use('/sale', require('./sale/postSale'))
-  app.use('/sale', require('./sale/getSale'))
-  app.use('/sale', require('./sale/getOneSale'))
-  app.use('/sale', require('./sale/putSale'))
-  app.use('/sale', require('./sale/deleteSale'))
-}
+  app.use('/sale', require('./sale/postSale'));
+  app.use('/sale', require('./sale/getSale'));
+  app.use('/sale', require('./sale/getOneSale'));
+  app.use('/sale', require('./sale/putSale'));
+  app.use('/sale', require('./sale/deleteSale'));
+};
 
 module.exports = routes;
